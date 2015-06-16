3d07fe4727faf34efa7a145080d3c95d5c25d96c
AlexBailey20 <arb4jr@virginia.edu>
2015-06-16 21:42:15 +0200

    Added very simple validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,11 @@ class Item < ActiveRecord::Base
 
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>"}
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+	validates(:name, {
+		:presence => true, 
+	})
+	validates(:description, :presence => true)
+	validates(:price, :presence => true)
+	validates(:avatar, :presence => true)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :pins
 	has_many :items, through: :pins
+
+	validates(:name, :presence => true)
+	validates(:user_name, :presence => true)
+	validates(:money, :presence => true)
 end
